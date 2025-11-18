5e8f0a2ce993c2d568d9aadabed430a409000796
Ceco1972 <tsvetan_k_ivanov@yahoo.com>
2025-11-18 10:54:14 +0200

    Create Context and UseReducer

---
+++ UseReducerExample/App.jsx
@@ -0,0 +1,31 @@
+import { useReducer } from "react";
+const initialState = { count: 0 };
+export function counterReducer(state, action) {
+    switch (action.type){
+        case "INCREMENT": 
+        return {count: state.count + 1};
+        case "DECREMENT":
+        return {count: state.count - 1};
+        case "RESET":
+        return {count: 0}; 
+        default:
+        return state;
+    }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(counterReducer, initialState)
+  return (
+    <div id="app">
+      <h1>The (Final?) Counter</h1>
+      <p id="actions">
+        <button onClick = {()=>dispatch({type: "INCREMENT"})}>Increment</button>
+        <button onClick = {()=>dispatch({type:"DECREMENT"})}>Decrement</button>
+        <button onClick={()=>dispatch({type:"RESET"})}>Reset</button>
+      </p>
+      <p id="counter">{state.count}</p>
+    </div>
+  );
+}
+
+export default App;
